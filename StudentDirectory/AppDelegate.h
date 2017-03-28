59a18a9d319ed5356908dc8771dd0e01e87ed17b
nicholaslee <nicholaslee@nicholaslees-MacBook-Pro.local>
2017-03-28 10:29:57 +0800

    Initial Commit

---
+++ StudentDirectory/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  StudentDirectory
+//
+//  Created by nicholaslee on 28/03/2017.
+//  Copyright © 2017 nicholaslee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ StudentDirectory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StudentDirectory
+//
+//  Created by nicholaslee on 28/03/2017.
+//  Copyright © 2017 nicholaslee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
